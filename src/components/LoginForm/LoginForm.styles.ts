da7e7fc518691ae5862ad0e631871bbfa2ee9331
sergij14 <sergi.jajanidze@gmail.com>
2022-04-08 15:36:39 +0400

    added form markup

---
+++ src/components/LoginForm/LoginForm.styles.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  font-size: 1.6rem;
+  color: white;
+  width: 400px;
+`;
+
+export const Header = styled.div`
+  background: linear-gradient(
+    180deg,
+    rgba(120, 81, 123, 1) 0%,
+    rgba(103, 70, 106, 1) 100%
+  );
+  box-shadow: 1px 1px 1px #8d6590 inset;
+  padding: 2rem;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  font-size: 1.4rem;
+  & > h3 {
+    text-shadow: 0px 1px 2px #000;
+  }
+`;
+
+export const Body = styled.form`
+  background-color: #f2f2f2;
+  color: #78517b;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  box-shadow: 0 4px 2px -2px #251327;
+`;
+
+export const Input = styled.div`
+  position: relative;
+`;
+
+export const InputIcon = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 12px;
+`;
+
+export const InputField = styled.input`
+  background-color: #f0eef0;
+  border-radius: 5px;
+  border: 1px #dfdfdf solid;
+  font-size: 1.2rem;
+  padding: 1.2rem 1.6rem;
+  outline: 1px #fbfbfb solid;
+  width: 100%;
+`;
+
+export const SubmitBtn = styled.button`
+  background: linear-gradient(
+    180deg,
+    rgba(120, 81, 123, 1) 0%,
+    rgba(103, 70, 106, 1) 100%
+  );
+  border-radius: 5px;
+  align-self: flex-start;
+  border: none;
+  outline: none;
+  font-size: 12px;
+  padding: 1rem 2rem;
+  color: white;
+  font-weight: bold;
+  text-shadow: 0px 1px 2px #000;
+`;
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  AppContainer,
+  Body,
+  Header,
+  Input,
+  InputField,
+  InputIcon,
+  SubmitBtn,
+} from "./LoginForm.styles";
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
+    <AppContainer>
+      <Header>
+        <h3>Login form</h3>
+      </Header>
+      <Body>
+        <Input>
+          <InputField type="text" placeholder="Username" />
+          <InputIcon src="user.svg" alt="" />
+        </Input>
+        <Input>
+          <InputField type="password" placeholder="Password" />
+          <InputIcon src="locked.svg" alt="" />
+        </Input>
+        <SubmitBtn type="submit">Log In</SubmitBtn>
+      </Body>
+    </AppContainer>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
